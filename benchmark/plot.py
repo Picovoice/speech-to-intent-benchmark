3553d4848bd2dd8a9185c489a0add6d230bc51fa
Alireza Kenarsari <alireza@picovoice.ai>
2019-08-28 15:37:59 -0700

    style changes on figure

--- benchmark/plot.py
+++ benchmark/plot.py
@@ -8,9 +8,9 @@ pv_kitchen = [0.9, 0.93, 0.96, 0.97, 0.97, 0.98, 0.98]
 
 snr = [6, 9, 12, 15, 18, 21, 24]
 
-plt.plot(snr, df_cafe, color='r', marker='o', label='Dialogflow (Cafe)')
-plt.plot(snr, df_kitchen, color='b', marker='o', label='Dialogflow (Kitchen)')
-plt.plot(snr, pv_cafe, color='r', linestyle='--', marker='o', label='Picovoice (Cafe)')
+plt.plot(snr, df_cafe, color='r', marker='x', label='Dialogflow (Cafe)')
+plt.plot(snr, df_kitchen, color='r', linestyle='--', marker='o', label='Dialogflow (Kitchen)')
+plt.plot(snr, pv_cafe, color='b', marker='x', label='Picovoice (Cafe)')
 plt.plot(snr, pv_kitchen, color='b', linestyle='--', marker='o', label='Picovoice (Kitchen)')
 plt.xlim(6, 24)
 plt.xlabel('SNR dB')
