b61dbc8b61c0beb2c825297f304b5f4f9b516c01
Alireza Kenarsari <alireza@picovoice.ai>
2020-02-03 08:50:42 -0800

    v1.3 update

--- benchmark/plot.py
+++ benchmark/plot.py
@@ -1,17 +1,18 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-df_cafe = [0.67, 0.69, 0.74, 0.76, 0.78, 0.79, 0.84]
-df_kitchen = [0.68, 0.72, 0.74, 0.77, 0.78, 0.79, 0.79]
+df_cafe = np.array([0.67, 0.69, 0.74, 0.76, 0.78, 0.79, 0.84])
+df_kitchen = np.array([0.68, 0.72, 0.74, 0.77, 0.78, 0.79, 0.79])
+df = (df_cafe + df_kitchen) / 2.
 
-pv_cafe = [0.85, 0.92, 0.92, 0.96, 0.96, 0.97, 0.97]
-pv_kitchen = [0.9, 0.93, 0.96, 0.97, 0.97, 0.98, 0.98]
+pv_cafe = np.array([0.88, 0.94, 0.96, 0.96, 0.96, 0.97, 0.98])
+pv_kitchen = np.array([0.93, 0.96, 0.97, 0.97, 0.97, 0.98, 0.98])
+pv = (pv_cafe + pv_kitchen) / 2.
 
 snr = [6, 9, 12, 15, 18, 21, 24]
 
-plt.plot(snr, df_cafe, color='r', marker='x', label='Dialogflow (Cafe)')
-plt.plot(snr, df_kitchen, color='r', linestyle='--', marker='o', label='Dialogflow (Kitchen)')
-plt.plot(snr, pv_cafe, color='b', marker='x', label='Picovoice (Cafe)')
-plt.plot(snr, pv_kitchen, color='b', linestyle='--', marker='o', label='Picovoice (Kitchen)')
+plt.plot(snr, df, color='r', marker='x', label='Dialogflow')
+plt.plot(snr, pv, color='b', marker='o', label='Picovoice')
 plt.xlim(6, 24)
 plt.xlabel('SNR dB')
 plt.ylim(0.6, 1)
