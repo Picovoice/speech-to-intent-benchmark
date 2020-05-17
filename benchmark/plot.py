20d9f39105f0096b0a7d40472aeed0306eb8a2bf
Phu-Quy Felicia Trinh <felicia.trinh@gmail.com>
2020-05-17 11:44:10 -0700

    updated results

--- benchmark/plot.py
+++ benchmark/plot.py
@@ -1,16 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-df_cafe = np.array([0.67, 0.69, 0.74, 0.76, 0.78, 0.79, 0.84])
-df_kitchen = np.array([0.68, 0.72, 0.74, 0.77, 0.78, 0.79, 0.79])
+aws_cafe = np.array([0.70, 0.80, 0.85, 0.86, 0.87, 0.87, 0.87])
+aws_kitchen = np.array([0.79, 0.83, 0.85, 0.86, 0.87, 0.87, 0.87])
+aws = (aws_cafe + aws_kitchen) / 2.
+
+df_cafe = np.array([0.63, 0.70, 0.75, 0.78, 0.79, 0.80, 0.81])
+df_kitchen = np.array([0.69, 0.73, 0.77, 0.80, 0.81, 0.81, 0.83])
 df = (df_cafe + df_kitchen) / 2.
 
-pv_cafe = np.array([0.86, 0.93, 0.95, 0.96, 0.97, 0.97, 0.97])
-pv_kitchen = np.array([0.9, 0.94, 0.96, 0.97, 0.97, 0.97, 0.97])
+pv_cafe = np.array([0.86, 0.92, 0.95, 0.96, 0.97, 0.97, 0.98])
+pv_kitchen = np.array([0.90, 0.96, 0.96, 0.96, 0.98, 0.98, 0.98])
 pv = (pv_cafe + pv_kitchen) / 2.
 
 snr = [6, 9, 12, 15, 18, 21, 24]
 
+plt.plot(snr, aws, color='g', marker='^', label='Amazon Lex')
 plt.plot(snr, df, color='r', marker='x', label='Dialogflow')
 plt.plot(snr, pv, color='b', marker='o', label='Picovoice')
 plt.xlim(6, 24)
